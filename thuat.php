92a3e3d8ee103cafe460188fb6ea4f02685d912c
Trần Thuật <71955928+mithuat@users.noreply.github.com>
2021-12-21 10:26:48 +0700

    Update thuat.php
    Cập nhật lần 1

--- thuat.php
+++ thuat.php
@@ -1,1 +1,195 @@
-Đây là phần của thuật
+<header>
+        <!-- Begin header main -->
+        <div class="header-main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="logo">
+                            <a href="index.php"><img src="./assets/images/logo-web-official.png" alt="Logo"></a>
+                        </div>
+                    </div>
+
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-10">
+                                <form action="#">
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="search"><i class="fas fa-search my-search"></i></span>
+                                        <input type="search" class="form-control" placeholder="Nhập bài hát, video, tên nghệ sĩ bạn cần tìm" aria-label="Search" aria-describedby="search">
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="col-2 my-icon">
+                                <i class="fas fa-history my-history"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-3 my-login">
+                        <p class="text-white login"><a class="text-white" href="#">Đăng nhập</a> / <a class="text-white" href="#">Đăng Ký</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End header main -->
+
+        <!-- Begin header bottom -->
+        <div class="header-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <ul class="nav my-nav my-2">
+                            <li class="nav-item"><a class="nav-link my-link txt" href="#">Xếp Hạng</a></li>
+                            <li class="nav-item"><a class="nav-link my-link txt" href="#">Video Clip</a></li>
+                            <li class="nav-item"><a class="nav-link my-link txt" href="#">Việt Nam</a></li>
+                            <li class="nav-item"><a class="nav-link my-link txt" href="#">US-UK</a></li>
+                            <li class="nav-item"><a class="nav-link my-link txt" href="#">Nhạc Hoa</a></li>
+                            <li class="nav-item"><a class="nav-link my-link txt" href="#">Nhạc Hàn</a></li>
+                            <li class="nav-item"><a class="nav-link my-link txt" href="#">Nhạc Nhật</a></li>
+                            <li class="nav-item"><a class="nav-link my-link txt" href="#">Nhạc Pháp</a></li>
+                            <li class="nav-item"><a class="nav-link my-link txt" href="#">Nhạc khác</a></li>
+                            <li class="nav-item my-1">
+                                <div class="dropdown my-dropdown">
+                                    <button class="btn btn-secondary btn-tran" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i class="fas fa-ellipsis-h my-dot"></i>
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                        <li><a class="dropdown-item" href="#">Action</a></li>
+                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link my-upload" href="#">
+                                    <i class="fas fa-cloud-upload-alt"></i>
+                                    Upload
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End header-bottom -->
+    </header>
+    <!-- End header -->
+
+    <!-- Begin banner -->
+    <div class="banner mx-20">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <img src="./assets/images/banner.jpg" alt="Banner">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End banner -->
+
+    <!-- Begin slider -->
+    <div class="slider px-10 py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                        <!-- <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        </div> -->
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <div class="row">
+
+                                    <div class="col-2 col-20">
+                                        <div class="box">
+                                            <div class="box-img">
+                                                <img src="./assets/images/devuong-single.jpg" alt="">
+                                            </div>
+                                            <div class="box-content">
+                                                <a class="txt txt-bold my-link" href="#">1. Đế Vương (Single)</a>
+                                                <a class="sub-txt my-link" href="#">Dunghoangpham</a>
+                                                <p class="txt-red">Lossless</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-2 col-20">
+                                        <div class="box">
+                                            <div class="box-img">
+                                                <img src="./assets/images/rapviet.jpg" alt="">
+                                            </div>
+                                            <div class="box-content">
+                                                <a class="txt txt-bold my-link" href="#">2. Rap Việt Session 2: Tập 9</a>
+                                                <a class="sub-txt my-link" href="#">Blacka</a>
+                                                <p class="txt-red">Lossless</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-2 col-20">
+                                        <div class="box">
+                                            <div class="box-img">
+                                                <img src="./assets/images/universe.jpg" alt="">
+                                            </div>
+                                            <div class="box-content">
+                                                <a class="txt txt-bold my-link" href="#">3. Universe - The 3rd Album</a>
+                                                <a class="sub-txt my-link" href="#">NCT</a>
+                                                <p class="txt-red">Lossless</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-2 col-20">
+                                        <div class="box">
+                                            <div class="box-img">
+                                                <img src="./assets/images/worldofwalker.jpg" alt="">
+                                            </div>
+                                            <div class="box-content">
+                                                <a class="txt txt-bold my-link" href="#">4. World Of Walker</a>
+                                                <a class="sub-txt my-link" href="#">Alan Walker</a>
+                                                <p class="txt-red">Lossless</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-2 col-20">
+                                        <div class="box">
+                                            <div class="box-img">
+                                                <img src="./assets/images/rapviet.jpg" alt="">
+                                            </div>
+                                            <div class="box-content">
+                                                <a class="txt txt-bold my-link" href="#">2. Rap Việt Session 2: Tập 8</a>
+                                                <a class="sub-txt my-link" href="#">Blacka</a>
+                                                <p class="txt-red">Lossless</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- <div class="carousel-item">
+                                <img src="./assets/images/rapviet.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="./assets/images/laaitubolaaivohinh-single.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="./assets/images/universe.jpg" class="d-block w-100" alt="...">
+                            </div> -->
+                        </div>
+                        <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Slider -->
